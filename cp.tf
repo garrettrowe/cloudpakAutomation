ae01aab39e8246844a5ad589a97eafd7f62bc178
Garrett Rowe <garrettrowe@yahoo.com>
2021-02-18 21:25:30 -0800

    Update cp.tf

--- cp.tf
+++ cp.tf
@@ -191,7 +191,7 @@ resource "ibm_container_addons" "addons" {
   } 
 }
 
-resource "null_resource" "oc_setup18" {
+resource "null_resource" "oc_setup19" {
   provisioner "local-exec" { 
     command = <<EOT
 export CPD_REGISTRY=cp.icr.io/cp/cpd
