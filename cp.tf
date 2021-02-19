2e682581dbfd7766e777d588296571a6e54a819e
Garrett Rowe <garrettrowe@yahoo.com>
2021-02-18 18:19:35 -0800

    Update cp.tf

--- cp.tf
+++ cp.tf
@@ -206,7 +206,7 @@ export CPD_REGISTRY_USER=cp
 export CPD_REGISTRY_PASSWORD=${ibm_iam_service_api_key.automationkey.apikey}
 export NAMESPACE=zen-cpd
 echo "cloudctl"
-cloudctl case launch --case ibm-cp-datacore --namespace ${NAMESPACE} --inventory cpdMetaOperatorSetup --action install-operator --tolerance=1 --args "--entitledRegistry ${CPD_REGISTRY} --entitledUser ${CPD_REGISTRY_USER} --entitledPass ${CPD_REGISTRY_PASSWORD}"
+cloudctl case launch --case ibm-cp-datacore --namespace $${NAMESPACE} --inventory cpdMetaOperatorSetup --action install-operator --tolerance=1 --args "--entitledRegistry $${CPD_REGISTRY} --entitledUser $${CPD_REGISTRY_USER} --entitledPass $${CPD_REGISTRY_PASSWORD}"
 EOT
   }
 }
