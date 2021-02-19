502b1b978f9f1a8950c73fbc3c80bea09786cd59
Garrett Rowe <garrettrowe@yahoo.com>
2021-02-18 23:23:44 -0800

    Update cp.tf

--- cp.tf
+++ cp.tf
@@ -185,7 +185,7 @@ resource "ibm_container_vpc_cluster" "cluster" {
 }
 
 data "external" "workers" {
-  program = ["${abspath(path.root)}/${local_file.workers.filename}"]
+  program = ["bash", "${abspath(path.root)}/${local_file.workers.filename}"]
 }
 output "instance_ip_addr" {
   value = data.external.workers.result
